f7a3a6a0baa50dfe291838777708b0bf1bf70584
Berdikulov-571 <bsanjarbek06@gmail.com>
2023-10-02 21:30:03 +0500

    created class User

---
+++ src/First/Task-5/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace First
+{
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
