9924756be9b8b8ca65806f12a550b9c6bc4b2147
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-10-02 21:44:58 +0500

    Task8

--- src/First/Task8/User.cs
+++ src/First/Task8/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace First.Task8
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string MyProperty { get; set; }
         public User() { }
 
     }
