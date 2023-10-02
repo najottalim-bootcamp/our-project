c9f1dda4c6ca650b34459da31555a94e761403a7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-10-02 21:45:17 +0500

    Added properties in Salohiddin on task-18

--- src/First/task-18/Salohiddin.cs
+++ src/First/task-18/Salohiddin.cs
@@ -2,5 +2,13 @@
 
 public class Salohiddin
 {
-    // Something
+    public long Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
 }
