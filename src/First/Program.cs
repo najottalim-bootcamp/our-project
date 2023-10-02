dce5b80761bb8e26cf2500a32196dce26fdfcb0b
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-02 21:38:47 +0500

    Class Added

--- src/First/Program.cs
+++ src/First/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!, Its Aziz's sms");
-Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+﻿using First.Task_4;
+
+Aziz_s.GetPerson();
---
+++ src/First/Task_4/Aziz's.cs
@@ -0,0 +1,14 @@
+﻿namespace First.Task_4
+{
+    public static class Aziz_s
+    {
+        public static string Name { get; set; } = "Aziz";
+        public static int Age { get; set; } = 17;
+        public static long Number { get; set; } = 998992704623;
+
+        public static void GetPerson()
+        {
+            Console.WriteLine(Name,Age,Number);
+        }
+    }
+}
