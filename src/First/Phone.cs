9169dc4705a3ba4459cf3af3e4fb06456ed8ace8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-10-02 21:13:15 +0500

    Phone cs

---
+++ src/First/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+        public int PhoneModel { get; set; }
+        public string PhoneName { get; set; }
+        public string PhoneColor { get; set; }
+
+
+
+    }
+}
