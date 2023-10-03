a42b13301217a987c76ca9488706365ef1177ea6
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-10-03 17:05:18 +0500

    cw added

--- src/First/task6/Task_6.cs
+++ src/First/task6/Task_6.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace First.task6
+﻿namespace First.task6
 {
-    internal class Task_6
+    public class Task_6
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.WriteLine("Nimgapalar???");
+        }
     }
 }
